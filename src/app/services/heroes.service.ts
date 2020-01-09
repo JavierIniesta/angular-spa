c7a037f14eb44e58047af1d2732a4dcd6d603911
Javier Iniesta <jiniesta@grupoarpada.com>
2020-01-09 12:04:06 +0100

    [FIX] Arreglar búsqueda

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -84,9 +84,11 @@ export class HeroesService {
     buscarHeroes(termino: string): Heroe[] {
       const heroesArr: Heroe[] = [];
       termino = termino.toLowerCase();
-      for (const heroe of this.heroes) {
+      for (let i = 0; i < this.heroes.length; i++) {
+        const heroe = this.heroes[i];
         const nombre = heroe.nombre.toLowerCase();
         if (nombre.indexOf(termino) >= 0) {
+          heroe.idx = i;
           heroesArr.push(heroe);
         }
       }
@@ -100,4 +102,5 @@ export interface Heroe {
     img: string;
     aparicion: string;
     casa: string;
+    idx?: number;
 }
